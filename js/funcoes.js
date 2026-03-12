751005c03e065f34e8ffda32cbe51fe7798db530
MontDeP <mauricio.monteiro@mail.uft.edu.br>
2026-03-12 09:08:59 -0300

    Correções
    Correções de código voltadas a estilização

--- js/funcoes.js
+++ js/funcoes.js
@@ -1,5 +1,4 @@
-function MinhaPrimeiraFuncao()
-{
+function MinhaPrimeiraFuncao() {
     $('#area-01').css({
         color: '#ff0000',
         textTransform: 'uppercase',
@@ -7,20 +6,16 @@ function MinhaPrimeiraFuncao()
     });
 }
 
-function MinhaSegundaFuncao()
-{
+function MinhaSegundaFuncao() {
     $('#area-02').empty();
 
-    var alunos = ['Aluno 01', 'Aluno02'];
+    var alunos = ['Aluno 01', 'Aluno 02'];
 
-    $.each(alunos,function(index, value){
-        $('#area-02').append(value);
+    $.each(alunos, function(index, value) {
+        $('#area-02').append(value + '<br>');
     });
 }
 
-function MinhaTerceiraFuncao()
-{
- {
-    
- }
+function MinhaTerceiraFuncao() {
+    $('#area-01').html('A terceira função foi executada com sucesso!');
 }
