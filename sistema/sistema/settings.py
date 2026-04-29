b7ae7f3f05ebf0b9ac67ca309d1d1203c7256710
MontDeP <mauricio.monteiro@mail.uft.edu.br>
2026-04-29 16:43:43 -0300

    k

--- sistema/sistema/settings.py
+++ sistema/sistema/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^)!7ck3ohk(cfo0^y_q7*y%xlh%b0pnxkqi3q1%(wb*48dmwkr'
+SECRET_KEY = 'django-insecure-!($3%9@$_4!0887nzo()-&6knl*!&*4=h_g-j8=3oo*d#eax+-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'sistema.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [ BASE_DIR / 'templates'],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -112,12 +112,11 @@ USE_I18N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR/'static'
+STATIC_ROOT = BASE_DIR / 'static'
 STATICFILES_DIRS = [BASE_DIR/'sistema'/'static']
 
 LOGIN_URL = '/'
--- sistema/veiculo/views.py
+++ sistema/veiculo/views.py
@@ -1,10 +1,10 @@
 from django.http import FileResponse, Http404
-from django.shortcuts import render
-from django.views.generic import ListView, DeleteView, UpdateView, CreateView
-from django.contrib.auth.mixins import LoginRequiredMixin
-from veiculo.models import Veiculo
-from .forms import FormularioVeiculo
+from django.shortcuts import get_object_or_404, render
 from django.urls import reverse_lazy
+from django.views.generic import CreateView, DeleteView, ListView, UpdateView, View
+from veiculo.models import Veiculo
+from veiculo.forms import FormularioVeiculo
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 class ListarVeiculos(LoginRequiredMixin, ListView):
     """
@@ -15,36 +15,48 @@ class ListarVeiculos(LoginRequiredMixin, ListView):
     template_name = 'veiculo/listar.html'
 
     def get_queryset(self, **kwargs):
-        pesquisa = self.request.GET.get('pesquisa' , None)
+        pesquisa = self.request.GET.get('pesquisa', None)
         queryset = Veiculo.objects.all()
         if pesquisa is not None:
             queryset = queryset.filter(modelo__icontains=pesquisa)
         return queryset
     
-class CriarVeiculos(LoginRequiredMixin, CreateView):
+class CriarVeiculo(LoginRequiredMixin,CreateView):
+    """
+    View para criar um novo veículo.
+    """
     model = Veiculo
     form_class = FormularioVeiculo
     template_name = 'veiculo/novo.html'
     success_url = reverse_lazy('listar-veiculos')
-    
-class EditarVeiculos(UpdateView):
+
+class EditarVeiculo(LoginRequiredMixin,UpdateView):
+    """
+    View para editar um veículo existente.
+    """
     model = Veiculo
     form_class = FormularioVeiculo
     template_name = 'veiculo/editar.html'
     success_url = reverse_lazy('listar-veiculos')
 
-class FotoVeiculo(ListView):
+class FotoVeiculo(LoginRequiredMixin,ListView):
+    """
+    View para exibir a foto de um veículo específico.
+    """
 
     def get(self, request, arquivo):
+        """
+        Método GET para retornar a foto do veículo.
+        """
         try:
-            veiculo = Veiculo.objects.get(foto='veiculo/fotos/{}'.format(arquivo))
+            veiculo = get_object_or_404(Veiculo, foto=arquivo)
             return FileResponse(veiculo.foto)
         except Veiculo.DoesNotExist:
-            raise Http404("Foto não encontrada ou acesso não autorizado")
+            raise Http404("Foto do veículo não encontrada.")
         except Exception as exception:
             raise exception
         
 class ExcluirVeiculo(LoginRequiredMixin, DeleteView):
     model = Veiculo
-    template_name = 'veiculo/excluir.html'
-    succes_url = reverse_lazy('listar-veiculos')
+    template_name = 'veiculo/excluir.html' 
+    success_url = reverse_lazy('listar-veiculos')
